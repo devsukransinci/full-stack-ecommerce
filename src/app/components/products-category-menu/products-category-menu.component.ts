1126f148e564aa7f1181c1e95762cbfd37f92510
sukrannsn <sukrannsn@outlook.com>
2024-06-11 13:14:49 +0300

    product-category-menu

--- src/app/components/products-category-menu/products-category-menu.component.ts
+++ src/app/components/products-category-menu/products-category-menu.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductCategory } from '../../common/product-category';
 
 @Component({
   selector: 'app-products-category-menu',
   templateUrl: './products-category-menu.component.html',
   styleUrl: './products-category-menu.component.css'
 })
-export class ProductsCategoryMenuComponent {
+export class ProductsCategoryMenuComponent implements OnInit {
 
+  productCategories:ProductCategory[]=[];
+
+  constructor(){
+
+  }
+
+  ngOnInit() {
+
+    
+  }
 }
